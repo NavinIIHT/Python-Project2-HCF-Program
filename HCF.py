5ad6fb44e7ce2c94f7d36dedd732e85bf3b76ffe
NavinIIHT <70938679+NavinIIHT@users.noreply.github.com>
2021-10-27 10:55:07 +0530

    Add files via upload

---
+++ HCF.py
@@ -0,0 +1,11 @@
+
+def get_hcf(x,y):
+    pass
+
+
+if __name__=="__main__":
+    pass
+
+
+
+#def get_hcf(x,y):\n\rpass if __name__=="__main__":\n\rpass 
---
+++ number_to_words.py
@@ -0,0 +1,15 @@
+
+def number_to_words(N):
+    pass
+
+
+if __name__=="__main__":
+    pass
+
+
+
+#def number_to_words(N):\n\rpass \n\r if __name__=="__main__":\n\rpass
+
+
+
+    
